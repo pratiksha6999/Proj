3567837a71a7bdd138f91479d080ff18627e864a
DELL <DELL@DESKTOP-GF3TMB9>
2022-10-13 15:41:22 +0530

    Dev1 added blogic...

--- DemoProj1/src/com/test/Testing.java
+++ DemoProj1/src/com/test/Testing.java
@@ -1,5 +1,11 @@
 package com.test;
 
+import com.beans.Utility;
+
 public class Testing {
 
+	public static void main(String[] args) {
+		Utility u=new Utility();
+		System.out.println(u.getDate());
+	}
 }
